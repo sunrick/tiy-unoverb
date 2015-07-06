a541c1f8f063c4d0df3d69a9411d9f656a5d30c5
Rickard Sunden <rickard.sunden@outlook.com>
2015-07-06 15:30:17 -0400

    Added users

---
+++ db/migrate/20150706192716_add_avatars_to_users.rb
@@ -0,0 +1,11 @@
+class AddAvatarsToUsers < ActiveRecord::Migration
+  
+  def up
+    add_attachment :users, :avatar
+  end
+
+  def down
+    remove_attachment :users, :avatar
+  end
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150706192716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "password"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+  end
 
 end
