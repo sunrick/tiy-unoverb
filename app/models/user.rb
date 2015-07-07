306175f4b084720a6faf8884f468a2471fc96fa5
Rickard Sunden <rickard.sunden@outlook.com>
2015-07-07 09:15:40 -0400

    Added user functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,26 @@
 class User < ActiveRecord::Base
+  
+  validates :password, :access_token, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :username, uniqueness: true, presence: true
+
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :avatar, content_type: ["image/jpg", "image/jpeg", "image/png"]
+
+  before_validation :ensure_access_token
+
+  def ensure_access_token
+    if self.access_token.blank?
+      self.access_token = User.generate_token
+    end
+  end
+
+  def self.generate_token
+    token = SecureRandom.hex
+    while User.exists?(access_token: token)
+      token = SecureRandom.hex
+    end
+    token
+  end
+
 end
