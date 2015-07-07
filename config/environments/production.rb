fc6202fd0daee43ee9f5b71e36ef16f6597dd584
Rickard Sunden <rickard.sunden@outlook.com>
2015-07-07 09:14:37 -0400

    Changed name of ENV variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV['S3_BUCKET'],
+      bucket: ENV['AWS_BUCKET'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
