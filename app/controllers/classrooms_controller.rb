34889f538b92839e47984e425a575ccd710e8461
Rickard Sunden <rickard.sunden@outlook.com>
2015-07-09 13:51:28 -0400

    Added create support

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,12 +1,24 @@
 class ClassroomsController < ApplicationController
+  before_action :authenticate_with_token!, only: [:create]
 
   def create
-    @language = Language.find_by(name: params[:language])
-    @classroom = current_user.classrooms.new( name: params[:name], 
-                                              description: params[:description], 
-                                              language: @language.id )
-    authorize! :create, @classroom
-    @classroom.save
+    @language = Language.find_by(name: params[:language].downcase)
+    if @language
+      @classroom = current_user.classrooms.new( name: params[:name], 
+                                                description: params[:description], 
+                                                language: @language,
+                                                code: params[:code] )
+      authorize! :create, @classroom
+      if @classroom.save
+        @role = Role.create( user: current_user, classroom: @classroom, role: "teacher" )
+        @user = current_user
+        render 'created.json.jbuilder', status: :created
+      else
+        render json: { errors: @classroom.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { message: "Language doesn't exist"}, status: :unprocessable_entity
+    end
   end
 
   def update
