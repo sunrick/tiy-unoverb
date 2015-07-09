0466298d7a450c4fea8e59eb127e5f14de45701a
Rickard Sunden <rickard.sunden@outlook.com>
2015-07-09 15:53:59 -0400

    Added columns to verb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709182155) do
+ActiveRecord::Schema.define(version: 20150709183554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(version: 20150709182155) do
     t.string   "pastparticiple_english"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "form"
+    t.string   "conjugation"
+    t.integer  "verb_id"
   end
 
   create_table "languages", force: :cascade do |t|
