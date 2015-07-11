152b713abeb8ed0bfb1e3b8370f38d7cafd82a78
Rickard Sunden <rickard.sunden@outlook.com>
2015-07-10 22:56:27 -0400

    Refactored unecessary code

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,14 +3,14 @@ class Ability
 
   def initialize(user)
     user ||= User.new # in case of guest
-    if user
-      can :create, Classroom
-      can :manage, Classroom do |classroom|
-        classroom.users.where(roles: {role: :teacher}).include? user
-      end
-      can :user_classrooms, Classroom do |classroom|
-        classroom.users.where(roles: {role: :student}).include? user
-      end
+
+    can :create, Classroom
+    can :manage, Classroom do |classroom|
+      classroom.users.where(roles: {role: :teacher}).include? user
     end
+    can :user_classrooms, Classroom do |classroom|
+      classroom.users.where(roles: {role: :student}).include? user
+    end
+    
   end
 end
