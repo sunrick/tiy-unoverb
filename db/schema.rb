0b25383bc068dee5285a079fd9841ac3e68cc20d
Rickard Sunden <rickard.sunden@outlook.com>
2015-07-14 23:12:03 -0400

    Removed code

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150715030512) do
+ActiveRecord::Schema.define(version: 20150715030834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20150715030512) do
     t.integer  "language_id"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
-    t.string   "code"
     t.integer  "roles_count",         default: 0
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
