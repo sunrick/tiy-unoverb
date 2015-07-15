5c4301e4c58053ebf644da32fbef37934c802743
Rickard Sunden <rickard.sunden@outlook.com>
2015-07-14 21:44:45 -0400

    Added counter_cache

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709183554) do
+ActiveRecord::Schema.define(version: 20150715012023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,9 +20,10 @@ ActiveRecord::Schema.define(version: 20150709183554) do
     t.string   "name"
     t.string   "description"
     t.integer  "language_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.string   "code"
+    t.integer  "roles_count", default: 0
   end
 
   create_table "conjugations", force: :cascade do |t|
