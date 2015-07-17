a8902b41dbd240cb3a42510f3592aef079fac914
Rickard Sunden <rickard.sunden@outlook.com>
2015-07-17 09:38:13 -0400

    Add tense and language association

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,4 +1,6 @@
 class Language < ActiveRecord::Base
   has_many :classrooms
   has_many :forms
+  has_many :tenses
+  has_many :verbs
 end
--- app/models/tense.rb
+++ app/models/tense.rb
@@ -1,4 +1,5 @@
 class Tense < ActiveRecord::Base
   has_many :conjugations
   has_many :verbs, through: :conjugations
+  belongs_to :language_id
 end
