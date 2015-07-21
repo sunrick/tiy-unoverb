ae1a2380f1c8c965dfc926b2d2b8286fa388d908
Rickard Sunden <rickard.sunden@outlook.com>
2015-07-21 10:26:23 -0400

    Added associations for exercise and solution

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,5 +2,6 @@ class Exercise < ActiveRecord::Base
   has_many :questions
   belongs_to :classroom
   has_many :games
+  has_many :solutions, through: :games
   validates :name, presence: true, uniqueness: true
 end
--- app/models/solution.rb
+++ app/models/solution.rb
@@ -1,6 +1,7 @@
 class Solution < ActiveRecord::Base
   belongs_to :game
   belongs_to :question
+  has_one :exercise, through: :game
   
   validates :time, numericality: { only_integer: true }
   validates :guess, presence: true
